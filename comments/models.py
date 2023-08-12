bb23859d3305122138e67fea9a956034a2d03d4e
byungyong <quddyddlrk@naver.com>
2023-08-12 15:54:20 +0900

    comment 작성일시 추가

--- comments/models.py
+++ comments/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Comment(models.Model):
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     # author, scedule 은 연결된 값인데 연결하는 것에 대한 건 추후에
     author = models.ForeignKey("users.User", on_delete=models.CASCADE)
