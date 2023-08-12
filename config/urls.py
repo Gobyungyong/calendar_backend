2bdb9d955386fcdf3ff5f5d8a67ae6a137482d11
byungyong <quddyddlrk@naver.com>
2023-08-12 14:59:58 +0900

    팀 내 닉네임 중복검사 API 작성

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/comments/", include("comments.urls")),
-    # path('api/v1/nicknames/', include('nicknames.urls')),
+    path("api/v1/nicknames/", include("nicknames.urls")),
     # path('api/v1/schedules/', include('schedules.urls')),
     path("api/v1/teams/", include("teams.urls")),
     path("api/v1/users/", include("users.urls")),
